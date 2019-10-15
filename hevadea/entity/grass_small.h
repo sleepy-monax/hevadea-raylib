49a5099e8b9ecbb5c034a1e2a2b14eacfc4868a8
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-10-15 13:26:04 +0200

    entities: Fixed sprite offset for grasses.

--- hevadea/entity/grass_small.h
+++ hevadea/entity/grass_small.h
@@ -8,7 +8,7 @@ void grass_small_entity_create(entity_instance_t *instance)
     instance->components = COMPONENT_SPRITE;
     instance->colider = (rectangle_t){-2, -2, 4, 4};
     instance->sprite = atlas_sprite_by_name("entity/weath0");
-    instance->sprite_origine = (vector_t){8, 8};
+    instance->sprite_origine = (vector_t){8, 10};
 }
 
 const entity_blueprint_t ENTITY_GRASS_SMALL = (entity_blueprint_t){
--- hevadea/entity/grass_tall.h
+++ hevadea/entity/grass_tall.h
@@ -8,7 +8,7 @@ void grass_tall_entity_create(entity_instance_t *instance)
     instance->components = COMPONENT_SPRITE;
     instance->colider = (rectangle_t){-2, -2, 4, 4};
     instance->sprite = atlas_sprite_by_name("entity/weath2");
-    instance->sprite_origine = (vector_t){8, 8};
+    instance->sprite_origine = (vector_t){8, 10};
 }
 
 const entity_blueprint_t ENTITY_GRASS_TALL = (entity_blueprint_t){
